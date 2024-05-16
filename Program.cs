9bd4519cd468c6ea905b19ab08501a042907feba
Lucas Monteiro <lucas_monteiro1995@hotmail.com>
2024-05-16 10:38:19 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone nokia: ");
+Smartphone nokia = new Nokia(numero: "123", modelo: "Modelo 01", imei: "111", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whats");
+
+Console.WriteLine("Smartphone Samsung: ");
+Smartphone iphone = new Iphone(numero: "124", modelo: "Modelo 02", imei: "112", memoria: 128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
